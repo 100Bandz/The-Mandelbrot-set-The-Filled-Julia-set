37cb72286e3b6fd240a5309cd941d204cad30f7f
Nico <nbl0357@gmail.com>
2021-05-04 17:47:25 -0400

    Iteration and drawing
    Completed the iteration of f(z) = z^2 + c
    for 100 iterations
    Then set the bitmap pixels values as the x,y coordinates

--- Mandelbrot set/Form1.cs
+++ Mandelbrot set/Form1.cs
@@ -16,5 +16,38 @@ namespace Mandelbrot_set
         {
             InitializeComponent();
         }
+
+        private void Form1_Shown(object sender, EventArgs e)    //Literally does whatever is codded, when the form is first shown
+        {
+            Bitmap bitmapImage = new Bitmap(pbNumberline.Width, pbNumberline.Height);
+
+            for (int x = 0; x < pbNumberline.Width; x++)
+            {
+                for (int y = 0; y < pbNumberline.Height; y++)
+                {
+                    double a = (double)(x - (pbNumberline.Width / 2)) / (double)(pbNumberline.Width / 4);
+                    double b = (double)(y - (pbNumberline.Height / 2)) / (double)(pbNumberline.Height / 4);
+
+                    ComplexNumber c = new ComplexNumber(a, b);
+                    ComplexNumber z = new ComplexNumber(0, 0);
+
+                    int iteration = 0;
+
+                    while (iteration < 100)
+                    {
+                        iteration++;
+                        z.square();
+                        z.add(c);
+                        if (z.magnitude() > 2.0)
+                        {
+                            break;
+                        }
+                    }
+                    bitmapImage.SetPixel(x, y, iteration < 100 ? Color.Black : Color.White);
+                }
+            }
+            pbNumberline.Image = bitmapImage;
+
+        }
     }
 }
