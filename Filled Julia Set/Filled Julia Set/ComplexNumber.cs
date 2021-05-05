b9c7cd4b171bb2c5d26b601a903fba89eb40cf41
Nico <nbl0357@gmail.com>
2021-05-05 02:44:51 -0400

    Complex num class
    Created a complex num class for the Filled Julia Set

---
+++ Filled Julia Set/Filled Julia Set/ComplexNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filled_Julia_Set
+{
+    class ComplexNumber
+    {
+        public double a;
+        public double b;
+    }
+}
